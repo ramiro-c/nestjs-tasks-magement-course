bfe0969d2b5e517886ac732ac64b75d29a4a6acc
ramiro <rami992009@gmail.com>
2022-03-03 22:57:21 -0300

    sign in captura el error si no se encuentra el usuario

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,10 +1,15 @@
-import { Injectable, UnauthorizedException } from '@nestjs/common';
+import {
+  Injectable,
+  NotFoundException,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 import { UsersRepository } from './users.repository';
 import * as bcrypt from 'bcrypt';
 import { JwtPayload } from './jwt-payload.interface';
 import { JwtService } from '@nestjs/jwt';
+import { User } from './user.entity';
 
 @Injectable()
 export class AuthService {
@@ -21,8 +26,13 @@ export class AuthService {
     authCredentialsDto: AuthCredentialsDto,
   ): Promise<{ accessToken: string }> {
     const { username, password } = authCredentialsDto;
+    let user: User;
 
-    const user = await this.usersRepository.findOne({ username });
+    try {
+      user = await this.usersRepository.findOne({ username });
+    } catch (e) {
+      throw new NotFoundException(`User: ${username} was not found`);
+    }
 
     if (user && (await bcrypt.compare(password, user.password))) {
       const payload: JwtPayload = { username };
